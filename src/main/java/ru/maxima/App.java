3ff794b055d56fa23c7d44f30cba2012d6b3c509
yatsenko.v <FDT3sRtwD>
2023-07-04 15:53:10 +0300

    commit

--- src/main/java/ru/maxima/App.java
+++ src/main/java/ru/maxima/App.java
@@ -1,13 +1,48 @@
 package ru.maxima;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.maxima.model.Person;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Person person = session.get(Person.class, 3);
+
+            session.delete(person);
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            sessionFactory.close();
+        }
     }
 }
+
+
+//            Person person = session.get(Person.class, 1);
+//            System.out.println(person.getName());
+//            System.out.println(person.getAge());
+
+//            Person person1 = new Person("Test1", 1);
+//            Person person2 = new Person("Test2", 2);
+//            Person person3 = new Person("Test3", 3);
+//
+//            session.save(person1);
+//            session.save(person2);
+//            session.save(person3);
+
--- src/main/java/ru/maxima/model/Person.java
+++ src/main/java/ru/maxima/model/Person.java
@@ -1,2 +1,51 @@
-package ru.maxima.model;public class Person {
+package ru.maxima.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Person")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
